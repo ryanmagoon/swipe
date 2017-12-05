cff64504ee51d868aa0fdd2c8a1821f095044c8f
Ryan Magoon <mr.magoon5@gmail.com>
2017-12-04 23:12:07 -0500

    add deck, convert app to pure function and make view iphone X compatible

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
-import Ball from './src/Ball'
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import Deck from './src/Deck'
 
 const DATA = [
   { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
@@ -10,22 +10,25 @@ const DATA = [
   { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
   { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
   { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
-  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
-];
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' }
+]
 
-export default class App extends React.Component {
-  render() {
-    return (
-        <View style={styles.container}>
-          <Ball></Ball>
-        </View>
-    );
-  }
-}
+const renderCard = item => <Text>{item.text}</Text>
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
-  },
-});
+  }
+})
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <Deck
+      data={DATA}
+      renderCard={renderCard}
+    />
+  </SafeAreaView>
+)
+
+export default App
