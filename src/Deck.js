942fe28c6980f9a0d5b753e72c67affa9db2f351
Ryan Magoon <mr.magoon5@gmail.com>
2017-12-04 23:10:58 -0500

    add deck component

---
+++ src/Deck.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Animated } from 'react-native'
+
+class Deck extends Component {
+  renderCards() {
+    return this.props.data.map(item => this.props.renderCard(item))
+  }
+
+  render() {
+    return <View>{this.renderCards()}</View>
+  }
+}
+
+export default Deck
