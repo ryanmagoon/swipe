62767d884ee0c03eddc48277bb9e409390727ebb
Ryan Magoon <mr.magoon5@gmail.com>
2017-12-08 22:09:48 -0500

    add card rotation

--- src/Deck.js
+++ src/Deck.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import {
   Animated,
+  Dimensions,
   PanResponder,
   View
 } from 'react-native'
+
+const SCREEN_WIDTH = Dimensions.get('window').width
 
 class Deck extends Component {
   constructor(props) {
@@ -21,13 +24,26 @@ class Deck extends Component {
     this.state = { panResponder, position }
   }
 
+  getCardStyle() {
+    const { position } = this.state
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg']
+    })
+
+    return {
+      ...this.state.position.getLayout(),
+      transform: [{ rotate }]
+    }
+  }
+
   renderCards() {
     return this.props.data.map((item, index) => {
       if (index === 0) {
         return (
           <Animated.View
             key={item.id}
-            style={this.state.position.getLayout()}
+            style={this.getCardStyle()}
             {...this.state.panResponder.panHandlers}
           >
             {this.props.renderCard(item)}
